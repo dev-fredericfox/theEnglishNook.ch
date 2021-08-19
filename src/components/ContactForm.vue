7cdd51087a7b96b5dee5760eb1a76a45c56cafd9
Frederic Fox <dev.frederic.fox@gmail.com>
2021-08-19 09:42:11 +0200

    Contact form fetch bug fix

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,127 +1,117 @@
 <template>
-  <form
-    id="contact"
-    action="/"
-  >
-    <fieldset :disabled="formStatus.disabled">
-      <div class="field is-horizontal">
-        
-        <div class="field-body">
-          <div class="field">
-            <p class="control is-expanded">
-              <input
-                class="input"
-                v-bind:class="{ 'is-danger': formValidation.fname }"
-                type="text"
-                placeholder="First Name"
-                v-model="form.fname"
-              />
-            </p>
-            <p v-if="formValidation.fname" class="help is-danger">
-              Required
-            </p>
-          </div>
-          <div class="field">
-            <p class="control is-expanded">
-              <input
-                class="input"
-                v-bind:class="{ 'is-danger': formValidation.lname }"
-                type="text"
-                placeholder="Last Name"
-                v-model="form.lname"
-              />
-            </p>
-            <p v-if="formValidation.lname" class="help is-danger">
-              Required
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div class="field is-horizontal">
-        <div class="field-label"></div>
-        <div class="field-body">
-          <div class="field is-expanded">
-            <p class="control is-expanded">
-              <input
-                class="input"
-                v-bind:class="{ 'is-danger': formValidation.email }"
-                type="tel"
-                placeholder="E-mail"
-                v-model="form.email"
-              />
-            </p>
-            <p v-if="formValidation.email" class="help is-danger">
-              Required
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div class="field is-horizontal">
-        
-        <div class="field-body">
-          <div class="field">
-            <div class="control">
-              <input
-                class="input"
-                v-bind:class="{ 'is-danger': formValidation.subject }"
-                type="text"
-                placeholder="Subject"
-                v-model="form.subject"
-              />
+  <div v-if="!formStatus.success">
+    <form id="contact" action="/">
+      <fieldset :disabled="formStatus.disabled">
+        <div class="field is-horizontal">
+          <div class="field-body">
+            <div class="field">
+              <p class="control is-expanded">
+                <input
+                  class="input"
+                  v-bind:class="{ 'is-danger': formValidation.fname }"
+                  type="text"
+                  placeholder="First Name"
+                  v-model="form.fname"
+                />
+              </p>
+              <p v-if="formValidation.fname" class="help is-danger">Required</p>
             </div>
-            <p v-if="formValidation.subject" class="help is-danger">
-              Required
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div class="field is-horizontal">
-        
-        <div class="field-body">
-          <div class="field">
-            <div class="control">
-              <textarea
-                class="textarea"
-                placeholder="How may I help you…?"
-                v-bind:class="{ 'is-danger': formValidation.message }"
-                v-model="form.message"
-              ></textarea>
-            </div>
-            <p v-if="formValidation.message" class="help is-danger">
-              Required
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div class="field is-horizontal">
-        <div class="field-label">
-          <!-- Left empty for spacing -->
-        </div>
-        <div class="field-body">
-          <div class="field">
-            <div class="control">
-              <button
-                class="g-recaptcha button is-info is-small"
-                :class="{'is-loading' : formStatus.disabled}"
-                data-sitekey="6LfeXgccAAAAAPeC-YrfS3bGHOgIMoYzROdNJ8Ur"
-                data-callback="validateForm"
-                data-action="submit"
-              >
-                Send
-              </button>
-              <p v-if="formStatus.error" class="help is-danger">
-              Something went wrong, please try sending the form again.
-            </p>
+            <div class="field">
+              <p class="control is-expanded">
+                <input
+                  class="input"
+                  v-bind:class="{ 'is-danger': formValidation.lname }"
+                  type="text"
+                  placeholder="Last Name"
+                  v-model="form.lname"
+                />
+              </p>
+              <p v-if="formValidation.lname" class="help is-danger">Required</p>
             </div>
           </div>
         </div>
-      </div>
-    </fieldset>
-  </form>
+
+        <div class="field is-horizontal">
+          <div class="field-body">
+            <div class="field is-expanded">
+              <p class="control is-expanded">
+                <input
+                  class="input"
+                  v-bind:class="{ 'is-danger': formValidation.email }"
+                  type="tel"
+                  placeholder="E-mail"
+                  v-model="form.email"
+                />
+              </p>
+              <p v-if="formValidation.email" class="help is-danger">Required</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="field is-horizontal">
+          <div class="field-body">
+            <div class="field">
+              <div class="control">
+                <input
+                  class="input"
+                  v-bind:class="{ 'is-danger': formValidation.subject }"
+                  type="text"
+                  placeholder="Subject"
+                  v-model="form.subject"
+                />
+              </div>
+              <p v-if="formValidation.subject" class="help is-danger">
+                Required
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="field is-horizontal">
+          <div class="field-body">
+            <div class="field">
+              <div class="control">
+                <textarea
+                  class="textarea"
+                  placeholder="How may I help you…?"
+                  v-bind:class="{ 'is-danger': formValidation.message }"
+                  v-model="form.message"
+                ></textarea>
+              </div>
+              <p v-if="formValidation.message" class="help is-danger">
+                Required
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="field is-horizontal">
+          <div class="field-body">
+            <div class="field">
+              <div class="control">
+                <button
+                  class="g-recaptcha button is-info is-small"
+                  :class="{ 'is-loading': formStatus.disabled }"
+                  data-sitekey="6LfeXgccAAAAAPeC-YrfS3bGHOgIMoYzROdNJ8Ur"
+                  data-callback="validateForm"
+                  data-action="submit"
+                  @click="loading()"
+                >
+                  Send
+                </button>
+                <p v-if="formStatus.error" class="help is-danger">
+                  Something went wrong, please try sending the form again.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </fieldset>
+    </form>
+  </div>
+  <div v-if="formStatus.success">
+    <p>WOW THE FORM WAS SENT LMAO</p>
+  </div>
   <!-- endNew -->
 
   <div class="children">
@@ -159,25 +149,39 @@ export default {
       disabled: false,
       sending: false,
       error: false,
-      sent: false
-    })
+      success: false,
+    });
 
     //FUNCTIONS
     ////Submission to the backend
     const postForm = async () => {
       const postReqSettings = {
         method: "POST",
-        header: { "Content-Type": "application/json" },
-        body: JSON.stringify(form),
+        mode: 'cors',
+        header: {
+          'Accept': "application/json",
+          'Content-Type': "application/json",
+        },
+          body: JSON.stringify({"username": "xyz"}),
+
       };
       try {
-        const response = await fetch("http://localhost:5000/test", postReqSettings)
-        console.log(response)
-      } catch {
-        formStatus.disabled = false
-        formStatus.sending = false
-        formStatus.error = true
-        console.log("Post failed")
+        console.log(postReqSettings);
+        const response = await fetch(
+          "http://localhost:5000/test",
+          postReqSettings
+        );
+        console.log(response.status);
+        if (response.status == 200) {
+          formStatus.success = true;
+        } else {
+          throw "Form sending failed";
+        }
+      } catch (error){
+        formStatus.disabled = false;
+        formStatus.sending = false;
+        formStatus.error = true;
+        console.log("Post failed. Reason:"+error);
       }
     };
     ////Validation handler
@@ -186,20 +190,22 @@ export default {
         if (form.value[el].length < 1) {
           formValidation[el] = true;
           console.log("true", formValidation[el]);
+          formStatus.disabled = false;
         } else {
           formValidation[el] = false;
           console.log("false", formValidation[el]);
         }
-      })
+      });
       if (Object.values(formValidation).every(isTrue) === true) {
-          formStatus.disabled = true
-          postForm()
+        postForm();
       }
-    }
+    };
 
-      const isTrue = (currentBool) => currentBool === false
+    const loading = function () {
+      formStatus.disabled = true;
+    };
 
-
+    const isTrue = (currentBool) => currentBool === false;
 
     //LIFECYCLE HOOKS
     onMounted(() => {
@@ -217,7 +223,8 @@ export default {
       form,
       formValidation,
       validateForm,
-      formStatus
+      loading,
+      formStatus,
     };
   },
 };
