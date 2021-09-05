cf20d9e7c63d4962ea1916eb2996d3f5c4c39d6e
Frederic Fox <dev.frederic.fox@gmail.com>
2021-09-05 16:58:09 +0200

    Custom deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,23 +6,20 @@ set -e
 # build
 npm run build
 
-# navigate into the build output directory
-cd dist
-
 # add .nojekyll to bypass GitHub Page's default behavior
-touch .nojekyll
+touch ./dist/.nojekyll
 
 # if you are deploying to a custom domain
 echo 'www.theenglishnook.ch' > CNAME
 
-git init
 git add -A
 git commit -m 'deploy'
+git subtree push --prefix dist origin gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+# git push -f git@github.com:dev-fredericfox/theEnglishNook.ch.git main:gh-pages
 
 cd -
